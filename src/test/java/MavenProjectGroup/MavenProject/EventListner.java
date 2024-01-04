a9b253bf04335d1c88915fda63ba41dc3260a78c
Manchana <sthamanchana@gmail.com>
2024-01-04 14:35:35 -0600

    Added eventlistener file

---
+++ src/test/java/MavenProjectGroup/MavenProject/EventListner.java
@@ -0,0 +1,36 @@
+package MavenProjectGroup.MavenProject;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class EventListner implements ITestListener {
+	@Override
+	public void onTestStart(ITestResult result) {
+		Reporter.log(result.getName() + " Started...");
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		Reporter.log(result.getName() + " Success...");
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		Reporter.log(result.getName() + " ...Failed");
+	}
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+		Reporter.log(result.getName() + " ...Failed with timeout");
+	}
+	@Override
+	public void onFinish(ITestContext context) {
+		Reporter.log(context.getName() + "...Finished");
+	}
+	
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		Reporter.log(result.getName() + "...Skipped");
+	  }
+}
